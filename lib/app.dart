3910cef856bc10c8285dc27b93e98511f9f3fa0d
viscosenpai <kama.hiroki@gmail.com>
2022-01-15 14:33:03 +0900

    [add] screens 各画面のファイルを追加

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:stack_up/screens/result/result.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Result();
+  }
+}
---
+++ lib/screens/result/result.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatefulWidget {
+  const Result({Key? key}) : super(key: key);
+
+  @override
+  State<Result> createState() => _ResultState();
+}
+
+class _ResultState extends State<Result> {
+  // boolean used to handle container animation which expands from the FAB
+  // FABから拡張されるコンテナアニメーションを処理するために使用されるブール値
+  bool clickedCentreFAB = false;
+  // to handle which item is currently selected in the bottom app bar
+  // 下部のアプリバーで現在選択されているアイテムを処理する
+  int selectedIndex = 0;
+  String text = "Home";
+
+  //call this method on click of each bottom app bar item to update the screen
+  // 画面を更新するには、下部の各アプリバーアイテムをクリックしてこのメ​​ソッドを呼び出します
+  void updateTabSelection(int index, String buttonText) {
+    setState(() {
+      selectedIndex = index;
+      text = buttonText;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(),
+      //specify the location of the FAB
+      // FABの場所を指定します
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: "Stack",
+        child: Container(
+          margin: const EdgeInsets.all(15.0),
+          child: const Icon(Icons.add),
+        ),
+        elevation: 4.0,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+              const SizedBox(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.home),
+              ),
+            ],
+          ),
+        ),
+        // to add a space between the FAB and BottomAppBar
+        // FABとBottomAppBarの間にスペースを追加します
+        shape: const CircularNotchedRectangle(),
+        // color of the BottomAppBar
+        // BottomAppBarの色
+        color: Colors.white,
+      ),
+    );
+  }
+}
